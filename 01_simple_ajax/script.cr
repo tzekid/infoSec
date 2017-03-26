82c920ef2f9cb37e01c97941bbf41a3083df2d1e
Mircea Ilie Ploscaru <tzekid2010@gmail.com>
2017-03-26 18:43:40 +0200

    minor fix

---
+++ 01_simple_ajax/script.cr
@@ -0,0 +1,16 @@
+#!/usr/bin/crystal
+# The compiled version (?only) works on linux
+require "http"
+
+# Take an argument (cmd line)
+name = ARGV.size == 1 ? ARGV[0] : "bob"
+
+LINK_TO_BOB_PHP = "http://localhost/infosec/01_xmlreq/bob.php"
+
+# Send request to URL
+response = HTTP::Client.get LINK_TO_BOB_PHP + "?q=#{name}"
+
+# Print response
+response = response.body
+print "Server response: `#{response}`"
+
--- 01_simple_ajax/script.rb
+++ 01_simple_ajax/script.rb
@@ -4,8 +4,10 @@ require 'curb'
 # Take an argument (cmd line)
 name = (ARGV[0]) ? ARGV[0] : "bob"
 
+LINK_TO_BOB_PHP = "http://localhost/infosec/01_xmlreq/bob.php"
+
 # Send request to URL
-c = Curl::Easy.perform("http://localhost/infosec/01_xmlreq/bob.php?q=#{name}")
+c = Curl::Easy.perform(LINK_TO_BOB_PHP + "?q=#{name}")
 
 # Print response
 response = c.body_str
